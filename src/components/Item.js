194a803bfcaad2ed9d0b7c2a491355be1d7435ae
ahmad dregat <ahmad.dregat.97@hotmail.com>
2021-07-08 17:38:54 +0300

    spot check 2,3 completed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,26 +1,29 @@
-import React, { Component } from 'react';
-import { observer } from 'mobx-react'
+import React, { Component } from "react";
+import { observer } from "mobx-react";
 
 class Item extends Component {
   checkItem = () => {
     //your code here
-  }
+  };
   editItem = () => {
     //your code here
-  }
+  };
   deleteItem = () => {
     //your code here
-  }
-  render() {
-    return (
-      <div className="">
-        {/*   your code here
+  };
+  /*   your code here
             each item should be in an input checkbox
             it should display the item name and location
             it should have both an edit button and a delete button
-      */}
-      </div>)
+      */
+  render() {
+    return (
+      <div className={this.props.store.item.completed ? "crossed" : null}>
+        <input type="checkbox" value={this.props.store.item.name} />
+        {this.props.store.item.name}
+      </div>
+    );
   }
 }
 
-export default observer(Item)
+export default observer(Item);
