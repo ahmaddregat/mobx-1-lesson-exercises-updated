523805b173337ac85ce0fe7ea1b670998950d31e
ahmad dregat <ahmad.dregat.97@hotmail.com>
2021-07-08 15:57:45 +0300

    spotcheck -1 complite

--- src/stores/ShoppingList.js
+++ src/stores/ShoppingList.js
@@ -1,26 +1,23 @@
 /* eslint-disable */
-import { observable, action, makeObservable } from 'mobx'
-import { Item } from './Item'
-
+import { observable, action, makeObservable } from "mobx";
+import { Item } from "./Item";
 
 export class ShoppingList {
-    constructor() {
-        this.list = []
-        this.length
-        // your code here
-
-    }
-    checkItem = () => {
-        // your code here
-    }
-    addItem = () => {
-        // your code here
-    }
-    editItem = () => {
-        // your code here
-    }
-    deleteItem = () => {
-        // your code here
-    }
+  constructor() {
+    this.list = [];
+    this.length;
+    makeObservable(this, { list: Observable, length: Observable });
+  }
+  checkItem = () => {
+    // your code here
+  };
+  addItem = () => {
+    // your code here
+  };
+  editItem = () => {
+    // your code here
+  };
+  deleteItem = () => {
+    // your code here
+  };
 }
-
